b3d59043b73df3a46e4db04dee78bbbd77892713
Ahmed M. Elhalaby <eng.ahmed.elhalaby@gmail.com>
2021-01-23 19:04:49 +0200

    =upload

--- app/Http/Controllers/General/ImageController.php
+++ app/Http/Controllers/General/ImageController.php
@@ -12,6 +12,7 @@ class ImageController extends Controller
 {
     public static function upload_single($request_file, $path,$type=1,$text=null)
     {
+        $path = 'storage/'.$path;
         if(!File::exists(public_path($path . '/org' ))){
             File::makeDirectory(public_path($path . '/org' ),0777,true);
         }
